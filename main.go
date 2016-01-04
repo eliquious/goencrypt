e51b091d7702a9a6fd28696e91eaee33b697774a
eliquious <eliquious@gmail.com>
2016-01-03 22:49:56 -0600

    Removed entity management commands

--- main.go
+++ main.go
@@ -34,32 +34,17 @@ var (
 	keyOutputPrefix = keyGenCmd.Arg("prefix", "Prefix of key files").Required().String()
 	keyOutputDir    = keyGenCmd.Flag("d", "Output directory of key files").Default(".").String()
 
-	// Base command for entity sub-commands
-	entityCmd        = app.Command("entity", "Generates a PGP entity")
-	entityOutputFile = entityCmd.Flag("o", "Output file of entity").Default("./entity.asc").String()
-	identityName     = entityCmd.Flag("name", "Name of identity").Default("").String()
-	identityComment  = entityCmd.Flag("comment", "Command for identity").Default("").String()
-	identityEmail    = entityCmd.Flag("email", "Email for identity").Default("").String()
-
-	// Creates a new entity
-	createEntityCmd = entityCmd.Command("new", "Create new entity")
-
-	// Adds an identity to an entity
-	addIdentityCmd            = entityCmd.Command("add", "Add an identity to the entity")
-	addIdentityFile           = addIdentityCmd.Arg("entity file", "Entity to add the identity to").Required().String()
-	addIdentityPrivateKeyFile = addIdentityCmd.Flag("key", "Private key of entity").String()
-
 	// Encrypts a file with a public key
-	encryptionCmd = app.Command("encrypt", "Encrypt a file")
+	encryptionCmd = app.Command("encrypt", "Encrypt from stdin")
 
 	// Signs a file with a private key
-	signCmd = app.Command("sign", "Sign a file")
+	signCmd = app.Command("sign", "Sign stdin")
 
 	// Verifies a file was signed with the public key
-	verifyCmd = app.Command("verify", "Verify a signature")
+	verifyCmd = app.Command("verify", "Verify a signature of stdin")
 
 	// Decrypts a file with a private key
-	decryptionCmd = app.Command("decrypt", "Decrypt a file")
+	decryptionCmd = app.Command("decrypt", "Decrypt from stdin")
 )
 
 func main() {
@@ -68,8 +53,8 @@ func main() {
 	// generate keys
 	case keyGenCmd.FullCommand():
 		generateKeys()
-	case createEntityCmd.FullCommand():
-		newEntity()
+	// case createEntityCmd.FullCommand():
+	// 	newEntity()
 	case encryptionCmd.FullCommand():
 		encryptFile()
 	case signCmd.FullCommand():
@@ -78,47 +63,6 @@ func main() {
 		verifyFile()
 	case decryptionCmd.FullCommand():
 		decryptFile()
-	case addIdentityCmd.FullCommand():
-		privKey := decodePrivateKey(*addIdentityPrivateKeyFile)
-
-		entityList := decodeKeyRing(*addIdentityFile)
-		var config packet.Config
-		for _, ent := range entityList {
-			ent.PrivateKey = privKey
-			addIdentity(false, config, ent)
-
-			// Sign identities
-			for _, id := range ent.Identities {
-				kingpin.Errorf("Identity: %#v", *ent.PrivateKey)
-				err := id.SelfSignature.SignUserId(id.UserId.Id, ent.PrimaryKey, ent.PrivateKey, nil)
-				if err != nil {
-					kingpin.FatalIfError(err, "Error signing identity: %s", err)
-					return
-				}
-			}
-
-			// Sign subkeys
-			for _, subkey := range ent.Subkeys {
-				err := subkey.Sig.SignKey(subkey.PublicKey, ent.PrivateKey, nil)
-				if err != nil {
-					kingpin.FatalIfError(err, "Error signing subkey: %s", err)
-				}
-			}
-
-			entityFile, err := os.Create(*entityOutputFile)
-			kingpin.FatalIfError(err, "Error creating entity file: %s", err)
-			defer entityFile.Close()
-
-			w, err := armor.Encode(entityFile, openpgp.PublicKeyType, map[string]string{})
-			err = ent.Serialize(w)
-			kingpin.Errorf("identities: %d", len(ent.Identities))
-			kingpin.Errorf("subkeys: %d", len(ent.Subkeys))
-			kingpin.FatalIfError(err, "Error serializing entity: %s", err)
-			w.Close()
-
-			break
-		}
-
 	default:
 		kingpin.FatalUsage("Unknown command")
 	}
@@ -256,7 +200,7 @@ func decryptFile() {
 
 	var entityList openpgp.EntityList
 	entityList = append(entityList, entity)
-	
+
 	md, err := openpgp.ReadMessage(block.Body, entityList, nil, nil)
 	kingpin.FatalIfError(err, "Error reading message")
 
@@ -347,150 +291,6 @@ func createEntityFromKeys(pubKey *packet.PublicKey, privKey *packet.PrivateKey) 
 		},
 	}
 	return &e
-}
-
-func newEntity() {
-	ent, err := openpgp.NewEntity(*identityName, *identityComment, *identityEmail, &packet.Config{
-		DefaultCipher:          packet.CipherAES256,
-		DefaultCompressionAlgo: packet.CompressionZLIB,
-		CompressionConfig: &packet.CompressionConfig{
-			Level: 9,
-		},
-		RSABits: *bits,
-	})
-	kingpin.FatalIfError(err, "Error creating entity: %s", err)
-
-	// Sign identities
-	for _, id := range ent.Identities {
-		err := id.SelfSignature.SignUserId(id.UserId.Id, ent.PrimaryKey, ent.PrivateKey, nil)
-		if err != nil {
-			kingpin.FatalIfError(err, "Error signing identity: %s", err)
-			return
-		}
-	}
-
-	// Sign subkeys
-	for _, subkey := range ent.Subkeys {
-
-		priv, err := os.Create(filepath.Join(*entityOutputFile + ".subkey.key"))
-		kingpin.FatalIfError(err, "Error writing private key to file: %s", err)
-		defer priv.Close()
-
-		encodePrivateKey(priv, subkey.PrivateKey.PrivateKey.(*rsa.PrivateKey))
-
-		err = subkey.Sig.SignKey(subkey.PublicKey, ent.PrivateKey, nil)
-		if err != nil {
-			kingpin.FatalIfError(err, "Error signing subkey: %s", err)
-		}
-	}
-
-	entityFile, err := os.Create(*entityOutputFile)
-	kingpin.FatalIfError(err, "Error creating entity file: %s", err)
-	defer entityFile.Close()
-
-	w, err := armor.Encode(entityFile, openpgp.PublicKeyType, map[string]string{})
-	err = ent.Serialize(w)
-	kingpin.Errorf("identities: %d", len(ent.Identities))
-	kingpin.Errorf("subkeys: %d", len(ent.Subkeys))
-	kingpin.FatalIfError(err, "Error serializing entity: %s", err)
-	w.Close()
-
-	priv, err := os.Create(filepath.Join(*entityOutputFile + ".key"))
-	kingpin.FatalIfError(err, "Error writing private key to file: %s", err)
-	defer priv.Close()
-
-	encodePrivateKey(priv, ent.PrivateKey.PrivateKey.(*rsa.PrivateKey))
-}
-
-// func createNewEntity() {
-// 	key, err := rsa.GenerateKey(rand.Reader, *bits)
-// 	kingpin.FatalIfError(err, "Error generating RSA key: %s", err)
-
-// 	currentTime := time.Now()
-// 	e := &openpgp.Entity{
-// 		PrimaryKey: packet.NewRSAPublicKey(currentTime, &key.PublicKey),
-// 		PrivateKey: packet.NewRSAPrivateKey(currentTime, key),
-// 		Identities: make(map[string]*openpgp.Identity),
-// 	}
-
-// 	var config packet.Config
-// 	addIdentity(true, config, e)
-
-// 	entityFile, err := os.Create(*entityOutputFile)
-// 	kingpin.FatalIfError(err, "Error creating entity file: %s", err)
-// 	defer entityFile.Close()
-
-// 	w, err := armor.Encode(entityFile, openpgp.PublicKeyType, map[string]string{})
-// 	e.Serialize(w)
-// 	w.Close()
-
-// 	priv, err := os.Create(filepath.Join(*entityOutputFile + ".key"))
-// 	kingpin.FatalIfError(err, "Error writing private key to file: %s", err)
-// 	defer priv.Close()
-
-// 	encodePrivateKey(priv, key)
-// }
-
-func addIdentity(isPrimary bool, config packet.Config, entity *openpgp.Entity) {
-	currentTime := config.Now()
-	uid := packet.NewUserId(*identityName, *identityComment, *identityEmail)
-
-	encryptingPriv, err := rsa.GenerateKey(rand.Reader, *bits)
-	kingpin.FatalIfError(err, "Error generating RSA key: %s", err)
-
-	entity.Identities[uid.Id] = &openpgp.Identity{
-		Name:   uid.Name,
-		UserId: uid,
-		SelfSignature: &packet.Signature{
-			CreationTime: currentTime,
-			SigType:      packet.SigTypePositiveCert,
-			PubKeyAlgo:   packet.PubKeyAlgoRSA,
-			Hash:         config.Hash(),
-			IsPrimaryId:  &isPrimary,
-			FlagsValid:   true,
-			FlagSign:     true,
-			FlagCertify:  true,
-			IssuerKeyId:  &entity.PrimaryKey.KeyId,
-		},
-	}
-
-	keyLifetimeSecs := uint32(86400 * 365 * 10)
-	subkey := openpgp.Subkey{
-		PublicKey:  packet.NewRSAPublicKey(currentTime, &encryptingPriv.PublicKey),
-		PrivateKey: packet.NewRSAPrivateKey(currentTime, encryptingPriv),
-
-		Sig: &packet.Signature{
-			CreationTime:              currentTime,
-			SigType:                   packet.SigTypeSubkeyBinding,
-			PubKeyAlgo:                packet.PubKeyAlgoRSA,
-			Hash:                      config.Hash(),
-			FlagsValid:                true,
-			FlagEncryptStorage:        true,
-			FlagEncryptCommunications: true,
-			IssuerKeyId:               &entity.PrimaryKey.KeyId,
-			KeyLifetimeSecs:           &keyLifetimeSecs,
-		},
-	}
-	subkey.PublicKey.IsSubkey = true
-	subkey.PrivateKey.IsSubkey = true
-	entity.Subkeys = append(entity.Subkeys, subkey)
-}
-
-func decodeKeyRing(filename string) openpgp.EntityList {
-
-	// open ascii armored private key
-	entityFile, err := os.Open(filename)
-	kingpin.FatalIfError(err, "Error opening entity: %s", err)
-	defer entityFile.Close()
-
-	// read key ring
-	entity, err := openpgp.ReadArmoredKeyRing(entityFile)
-	kingpin.FatalIfError(err, "Error reading entity: %s", err)
-
-	for _, e := range entity {
-		kingpin.Errorf("Entity: %#v", *e)
-	}
-	return entity
 }
 
 func generateKeys() {
